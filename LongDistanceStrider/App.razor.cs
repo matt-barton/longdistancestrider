da8bc1c960e1434cfa23836037a60f71f86fddda
matt <matt@mattbarton.org>
2026-03-28 00:51:11 +0000

    Deleted old project

--- LongDistanceStrider/App.razor.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Components;
-
-namespace LongDistanceStrider.Components
-{
-    public partial class App: ComponentBase
-    {
-
-    }
-}
--- LongDistanceStrider/Configuration/Settings.cs
+++
@@ -1,9 +0,0 @@
-public class Settings
-{
-    public DatabaseSettings? Database { get; set; }
-}
-
-public class DatabaseSettings
-{
-    public string? ConnectionString {get; set; }
-}
