5e319885c2d14abee01cbb6b1b72323585bcb178
Matt Barton <matt@mattbarton.org>
2026-03-31 12:02:53 +0100

    New backups

---
+++ db/backups/2026-03-31/Parameters.sql
@@ -0,0 +1,3 @@
+INSERT INTO [lds-db].dbo.Parameters (Name, Value) VALUES (N'CurrentYear', N'2026');
+INSERT INTO [lds-db].dbo.Parameters (Name, Value) VALUES (N'FirstYear', N'2025');
+INSERT INTO [lds-db].dbo.Parameters (Name, Value) VALUES (N'LastUpdated', N'2026-03-27T00:14:00');
